4c7142d02bc426b804db768058e1e5c13346a7d7
nap3831 <nap3831@gmail.com>
2019-10-28 22:53:46 +0700

    fixed error get api

--- src/Pages/News.js
+++ src/Pages/News.js
@@ -5,7 +5,7 @@ class News extends Component {
         return (
             <React.Fragment>
                 <div className="container-fluid">
-                        <Banner images={"https://images.unsplash.com/photo-1506629082955-511b1aa562c8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"} text={"News"}/>
+                        <Banner images={"https://images.unsplash.com/photo-1506629082955-511b1aa562c8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"} text={"News"} alt="image banner"/>
                         </div>
                 <div className="container-fluid MarginTopClass">
                     <div className="row">
