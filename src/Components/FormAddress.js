b368c0f8e1e4bfd769ee064e33f78265b902ed6d
nap3831 <nap3831@gmail.com>
2019-12-30 02:46:21 +0700

    create user interface and implement function add address

---
+++ src/Components/FormAddress.js
@@ -0,0 +1,83 @@
+import React, { Component } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTimes } from '@fortawesome/fontawesome-free-solid';
+import '../Assets/css/FormAddress.css';
+class FormAddress extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            nameAndLastName:null,
+            address:null,
+            tel:null
+        }
+        this.handleNameAndLastName = this.handleNameAndLastName.bind(this);
+        this.handleAddress = this.handleAddress.bind(this);
+        this.handleTelephone = this.handleTelephone.bind(this);
+    }
+    handleNameAndLastName = (e) =>{
+        this.setState({
+            nameAndLastName : e.target.value
+        })
+    }
+    handleAddress = (e) =>{
+        this.setState({
+            address : e.target.value
+        })
+    }
+    handleTelephone = (e) =>{
+        this.setState({
+            tel : e.target.value
+        })
+    }
+    render() {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col-12 FormAddressBox">
+                        <div className="container py-4">
+                            <div className="row">
+                                <div className="col-6 title">
+                                    ที่อยู่ของฉัน
+                                </div>
+                                <div className="col-6 text-right">
+                                    <FontAwesomeIcon className="bg-symbol" icon={faTimes} size="2x" onClick={this.props.handleClickAddress} />
+                                </div>
+                            </div>
+                        </div>
+                        <div className="underline my-2"></div>
+                        <div className="container">
+                            <div className="row my-3  field">
+                                <div className="form-group col-12">
+                                    <label className="text-center" htmlFor="nameAndLastName">ชื่อ - นามสกุล</label>
+                                    <input type="text" className="form-control" id="nameAndLastName" onChange={this.handleNameAndLastName}/>
+                                </div>
+                                <div className="form-group col-12 ">
+                                    <label className="text-center" htmlFor="address">ที่อยู่จัดส่ง</label>
+                                    <input type="text" className="form-control" id="address" onChange={this.handleAddress}/>
+                                </div>
+                                <div className="form-group col-12 " >
+                                    <label className="text-center" htmlFor="telephone">เบอร์ติดต่อ</label>
+                                    <input type="tel" className="form-control" id="telephone" onChange={this.handleTelephone} />
+                                </div>
+                                <div className="container">
+                                    <div className="row">
+                                        <div className="underline mt-2"></div>
+                                    </div>
+                                    <div className="row">
+                                        <div className="col-12  text-right">
+                                            <button className="btn buttonCalcel text-pink border-pink mt-3" onClick={this.props.handleClickAddress}>ยกเลิก</button>
+                                            <button className="btn buttonChange mt-3" onClick={()=>this.props.addAddress(this.state.nameAndLastName,this.state.address,this.state.tel)}>เพิ่มที่อยู่</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default FormAddress
--- src/Pages/ManageMemberAddress.js
+++ src/Pages/ManageMemberAddress.js
@@ -1,26 +1,68 @@
 import React, { Component } from 'react';
-import '../Assets/css/Purchase.css';
 import { connect } from 'react-redux';
-import Member from './Member'
-
-class ManageMemberAddress  extends Component {
+import Member from './Member';
+import { faShippingFast } from '@fortawesome/fontawesome-free-solid';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import '../Assets/css/ManageMemberAddress.css';
+import FormAddress from '../Components/FormAddress';
+class ManageMemberAddress extends Component {
     constructor(props) {
         super(props)
-        this.state = {}
+        this.state = {
+            isAddedAddress: false,
+            nameAnaLastName: null,
+            address: null,
+            tel:null,
+        }
+        this.handleClickAddAddress = this.handleClickAddAddress.bind(this);
+    }
+    handleClickAddAddress = () => {
+        this.setState({
+            isAddedAddress: !this.state.isAddedAddress
+        })
+    }
+    handleAdddedAddress = (nameAnaLastname,address,tel) =>{
+        alert("ดำเนินการสำเร็จ")
+        this.setState({
+            nameAnaLastName:nameAnaLastname,
+            address:address,
+            tel:tel
+        })
+        this.handleClickAddAddress();
     }
     render() {
         return (
             <Member>
                 <div className="container-fluid ">
                     <div className="row">
-                    <div className="col-md-12 mt-5 col-12">
-                            <h3 className="font-weight-bold">ที่อยู่ของฉัน</h3>          
+                        <div className="col-12 my-5">
+                            <h3 className="font-weight-bold">ที่อยู่ของฉัน</h3>
                         </div>
-                        <div className="col-md-12 mt-3 col-12">
-                            Content
+                        <div className="col-12 my-5">
+                            <div className="container">
+                                <div className="row text-center mt-5">
+                                    <div className="col-12  pt-4">
+                                        <FontAwesomeIcon className="iconCar" icon={faShippingFast} size="10x" />
+                                    </div>
+                                    <div className="col-12    pt-3">
+                                        {this.state.nameAnaLastName && this.state.address && this.state.tel != null ?
+                                        <div>
+                                        {this.state.nameAnaLastName} <br/>
+                                        {this.state.address} <br/>
+                                        {this.state.tel}
+                                        </div>:
+                                        <p className="Text">ไม่มีข้อมูลที่อยู่</p>
+                                        }
+                                    </div>
+                                    <div className="col-12 ">
+                                        <button className="btn btnAddAddress" onClick={this.handleClickAddAddress}>เพิ่มที่อยู่</button>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
+                {this.state.isAddedAddress && <FormAddress handleClickAddress={this.handleClickAddAddress} addAddress={this.handleAdddedAddress}/>}
             </Member>
         )
     }
